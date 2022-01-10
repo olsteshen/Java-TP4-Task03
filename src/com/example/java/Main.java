729e8dc25e5e4d4d488b2abfd73695efb28f9556
stesho01 <Listo4ek>
2022-01-10 12:41:49 +0200

    task 03 - rework

--- src/com/example/java/Main.java
+++ src/com/example/java/Main.java
@@ -4,5 +4,15 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
+        HashMap<Integer, String> hmap = new HashMap<>();
+        hmap.put(1, "Anna");
+        hmap.put(2, "Alex");
+        hmap.put(3, "Betty");
+        hmap.put(4, "John");
+        hmap.put(5, "John");
+
+        InvertMap imap = new InvertMap();
+        System.out.println("Initial map:" + hmap);
+        System.out.println("Value of reversed " + imap.invert(hmap));
     }
 }
